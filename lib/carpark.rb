eea27337fbefcfaa4b7409ae3006d7ea160e0dd8
Jakukyo Friel <weakish@gmail.com>
2014-12-23 22:21:47 +0800

    Add cons, nth, nthcdr, setcar, setcdr.

--- lib/carpark.rb
+++ lib/carpark.rb
@@ -1,20 +1,43 @@
 class Array
   def method_missing(m, *a)
     s = m.to_s
-    return super.method_missing(m, a) unless s =~ /c[ad]+r/
-    o = s[1..-2]
-    
-    return self[0].send("c#{o[0..-2]}r".to_sym) if o[-1..-1]=="a"
-    return self[1..-1].send("c#{o[0..-2]}r".to_sym) if o[-1..-1]=="d"
-    
-    return "failed #{o}"
+    if [:cons, :setcar, :setcdr].include?(s)
+      self.send(s, a)
+    elsif s =~ /c[ad]+r/
+      o = s[1..-2]
+      return self[0].send("c#{o[0..-2]}r".to_sym) if o[-1..-1]=="a"
+      return self[1..-1].send("c#{o[0..-2]}r".to_sym) if o[-1..-1]=="d"
+      return "failed #{o}"
+    else
+      return super.method_missing(m, a) unless s =~ /c[ad]+r/
+    end
   end
-  
+
+  def cons(a)
+    return ([] << a) + self
+  end
+
   def car
     return self.first
   end
-  
+
   def cdr
     return self[1..-1]
+  end
+
+  def nth(n)
+    return self[n]
+  end
+
+  def nthcdr(n)
+    return self[n..-1]
+  end
+
+  def setcar(a)
+    return self[0] = a
+  end
+
+  def setcdr(l)
+    return self[1..-1] = l
   end
 end
--- tests/original_test.rb
+++ tests/original_test.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
-require File.join(File.dirname(__FILE__), 'test_helper')
+
+require_relative '../lib/carpark'
 
 class CarparkTest < Test::Unit::TestCase
+	def test_cons
+		assert_equal [1, 2, 3].cons(0), [0, 1, 2, 3] 
+	end
+
 	def test_car
 		assert_equal [1, 2, 3].car, 1
 	end
@@ -16,5 +21,31 @@ class CarparkTest < Test::Unit::TestCase
 	
 	def test_cdar
 		assert_equal [[1, 2], 3].cdar, [2]
+	end
+
+	def test_nth
+		assert_equal [1, 2, 3].nth(0), 1
+		assert_equal [1, 2, 3].nth(1), 2
+		assert_equal [1, 2, 3].nth(2), 3
+		assert_equal [1, 2, 3].nth(3), nil
+	end
+
+	def test_nthcdr
+		assert_equal [1, 2, 3].nthcdr(0), [1, 2, 3]
+		assert_equal [1, 2, 3].nthcdr(1), [2, 3]
+		assert_equal [1, 2, 3].nthcdr(2), [3]
+		assert_equal [1, 2, 3].nthcdr(3), []
+	end
+
+	def test_setcar
+		array = [1, 2, 3]
+		array.setcar(0)
+		assert_equal array, [0, 2, 3]
+	end
+
+	def test_setcdr
+		array = [1, 2, 3]
+		array.setcdr([4, 5])
+		assert_equal array, [1, 4, 5]
 	end
 end
